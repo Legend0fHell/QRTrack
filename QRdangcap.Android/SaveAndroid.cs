e248901e68434a3acc4da38e73691fb13b8ba423
Laptop <pnquang05@gmail.com>
2021-11-14 02:36:29 +0700

    D042 - 2 @ 14.11.2021

--- QRdangcap.Android/SaveAndroid.cs
+++ QRdangcap.Android/SaveAndroid.cs
@@ -1,4 +1,5 @@
-﻿using Java.IO;
+﻿using Android.Widget;
+using Java.IO;
 using System;
 using System.IO;
 using Xamarin.Essentials;
@@ -11,23 +12,30 @@ internal class SaveAndroid : ISave
     [Obsolete]
     public async void SaveAndView(string fileName, String contentType, MemoryStream stream)
     {
-        string root = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
-        Java.IO.File myDir = new Java.IO.File(root);
-        myDir.Mkdir();
-        Java.IO.File file = new Java.IO.File(myDir, fileName);
-        if (file.Exists()) file.Delete();
-        FileOutputStream outs = new FileOutputStream(file);
-        outs.Write(stream.ToArray());
-        outs.Flush();
-        outs.Close();
-        if (file.Exists())
+        try
         {
-            Android.Net.Uri path = Android.Net.Uri.FromFile(file);
-            await Launcher.OpenAsync(new OpenFileRequest
+            string root = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            Java.IO.File myDir = new Java.IO.File(root);
+            myDir.Mkdir();
+            Java.IO.File file = new Java.IO.File(myDir, fileName);
+            if (file.Exists()) file.Delete();
+            FileOutputStream outs = new FileOutputStream(file);
+            outs.Write(stream.ToArray());
+            outs.Flush();
+            outs.Close();
+            if (file.Exists())
             {
-                File = new ReadOnlyFile(path.Path),
-                Title = "Mở báo cáo bằng ứng dụng.."
-            });
+                Android.Net.Uri path = Android.Net.Uri.FromFile(file);
+                await Launcher.OpenAsync(new OpenFileRequest
+                {
+                    File = new ReadOnlyFile(path.Path),
+                    Title = "Mở báo cáo bằng ứng dụng.."
+                });
+            }
+        }
+        catch
+        {
+            Android.Widget.Toast.MakeText(Android.App.Application.Context, "Có lỗi xảy ra.", ToastLength.Short).Show();
         }
     }
 }
